f50b2a014e443f6abebf01dcee27be95e167af07
maxarvid <max.arvid.anderson@gmail.com>
2022-03-22 10:46:35 +0100

    removes comments from rails helper configures RSpec documentation

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,7 @@ module DgmsNewsAdmin
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Generators settings should go here.
   end
 end
